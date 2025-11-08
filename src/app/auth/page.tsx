799d7ed687f2888ff71bc116644f816ed40ac56c
Dreta <32196824+Dreta@users.noreply.github.com>
2025-11-08 21:25:03 +0800

    chore: update log in wording

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -4,8 +4,8 @@ import Branding from '@/app/lib/Branding'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGlobeAsia } from '@fortawesome/free-solid-svg-icons'
 
-export default async function AuthPage({searchParams}: { searchParams: never }) {
-    const {t} = await useTranslation('auth')
+export default async function AuthPage({ searchParams }: { searchParams: never }) {
+    const { t } = await useTranslation('auth')
     const back = (await searchParams)['redirect'] || '/'
     const redirect = `https://passport.seiue.com/authorize?response_type=token&client_id=${process.env.SEIUE_CLIENT_ID}&school_id=452&scope=reflection.read_basic&redirect_uri=${encodeURIComponent(`${process.env.HOSTED}/auth/callback?redirect=${encodeURIComponent(back)}`)}`
 
@@ -17,12 +17,16 @@ export default async function AuthPage({searchParams}: { searchParams: never }) 
             <div className="flex flex-col gap-3 w-full mb-5">
                 <a href={redirect}
                    className="flex items-center gap-3 bg-gray-50 rounded  dark:bg-gray-800 p-3 hover:bg-gray-100 dark:hover:bg-gray-700 transition-all duration-200">
-                    <img alt="" src="/images/seiue.webp" className="w-8 h-8 rounded-full"/>
+                    <div className="bg-white rounded-full flex items-center justify-center w-8 h-8">
+                        <img alt="" src="/images/seiue.webp" className="w-6 h-6 rounded-full"/>
+                    </div>
                     <p>{t('loginSeiue')}</p>
                 </a>
                 <Link href={`/auth/code?redirect=${encodeURIComponent(back)}`}
                       className="flex items-center gap-3 bg-gray-50 rounded  dark:bg-gray-800 p-3 hover:bg-gray-100 dark:hover:bg-gray-700 transition-all duration-200">
-                    <FontAwesomeIcon icon={faGlobeAsia} className="w-8 h-8"/>
+                    <div className="text-white bg-blue-500 rounded-full flex items-center justify-center w-8 h-8">
+                        <FontAwesomeIcon icon={faGlobeAsia}/>
+                    </div>
                     <p>{t('loginAccessCode')}</p>
                 </Link>
             </div>
